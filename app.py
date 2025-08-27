543ef81635cd6369abd8fd7b4f6aaeae2025d2c6
[yamashitaryoya] <[ryoyan1022@gmail.com]>
2025-08-27 18:22:24 +0900

    Add app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 import random
+from datetime import datetime
+
 
 # -----------------------------
 # Google スプレッドシート認証
